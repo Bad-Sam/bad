5ac214fb8b006a680e9f778512bfa3066b8f6535
Bad-Sam <sami.amara@pm.me>
2022-01-21 23:11:55 +0100

    Fixed typo in comment

--- src/bad/detect/isa.h
+++ src/bad/detect/isa.h
@@ -1,7 +1,7 @@
 #ifndef BAD_ISA_H
 #define BAD_ISA_H
 
-// Identity instruction set: x64, x86, ARM or PowerPC
+// Identify instruction set: x64, x86, ARM or PowerPC
 #if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
 #   define BAD_x64
 #elif defined(_M_IX86) || defined(_X86_) || defined(__i386__) || defined(__i386) || defined(i386)
