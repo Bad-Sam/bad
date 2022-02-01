83a1c1863c11b3da496baf9929a811661a70a044
Bad-Sam <sami.amara@pm.me>
2022-02-01 22:45:32 +0100

    Moved mathematical f32x4 function tests to new files

--- test/tests/test_f32x4.h
+++ test/tests/test_f32x4.h
@@ -6,6 +6,8 @@
 BAD_NAMESPACE_START
 
 // ============= Defaults =============
+//void test_f32x4_any();
+//void test_f32x4_zero();
 void test_f32x4_one();
 
 
@@ -14,31 +16,8 @@ void test_f32x4_load_store();
 void test_f32x4_loadu_storeu();
 
 
-// =========== Arithmetic ===========
-void test_f32x4_hadd();
-
-
-// ====== Advanced arithmetic =======
-void test_f32x4_abs();
-void test_f32x4_neg();
-void test_f32x4_mod();
-void test_f32x4_trunc();
-void test_f32x4_round();
-void test_f32x4_floor();
-void test_f32x4_ceil();
-void test_f32x4_clamp();
-
-void test_f32x4_mul_add();
-void test_f32x4_mul_sub();
-void test_f32x4_nmul_add();
-void test_f32x4_nmul_sub();
-
-
 // ======= Selection & tests ========
 void test_f32x4_blend();
-void test_f32x4_is_nan();
-void test_f32x4_is_infinite();
-void test_f32x4_is_finite();
 
 BAD_NAMESPACE_END
 
