f551523862820cc91c01081157f84f96e3bcb93b
Bad-Sam <sami.amara@pm.me>
2022-02-03 16:44:25 +0100

    Appended extra tests on f32x4_calc.h and mask128.h

--- test/main.cpp
+++ test/main.cpp
@@ -12,21 +12,36 @@ using namespace bad;
 
 int main()
 {
-    // mask128
+    fprintf(stderr, "========== mask128.h ==========");
     test_mask128_load_store();
     test_mask128_loadu_storeu();
+    test_mask128_set();
+    test_mask128_set1();
+    test_mask128_zero();
     test_mask128_all1();
     test_mask128_value32();
     test_mask128_highbit32();
     test_mask128_lowbit32();
     test_mask128_exponent32();
-
-    // f32x4
+    test_mask128_eq();
+    test_mask128_neq();
+    test_mask128_and();
+    test_mask128_and_not();
+    test_mask128_or();
+    test_mask128_xor();
+    test_mask128_not();
+    
+    fprintf(stderr, "\n\n========== f32x4.h ==========");
+    test_f32x4_zero();
     test_f32x4_one();
     test_f32x4_load_store();
     test_f32x4_loadu_storeu();
+    test_f32x4_set();
+    test_f32x4_set1();
     test_f32x4_blend();
+    test_f32x4_cast_mask128();
     
+    fprintf(stderr, "\n\n========== f32x4_calc.h ==========");
     test_f32x4_hadd();
     test_f32x4_abs();
     test_f32x4_neg();
