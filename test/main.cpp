f5f0c852923af7fe276b605a6db824c73c203dae
Bad-Sam <sami.amara@pm.me>
2022-02-05 23:31:06 +0100

    Added all new test functions to the list of unit tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,11 @@
 #include "test.h"
-
 #include "tests/test_mask128.h"
 #include "tests/test_mask256.h"
 #include "tests/test_f32x4.h"
 #include "tests/test_f32x8.h"
 #include "tests/test_f32x4_calc.h"
+#include "tests/test_f32x8_calc.h"
+
 
 #if defined(__cplusplus)
 using namespace bad;
@@ -73,6 +74,7 @@ int main()
     fprintf(stderr, "\n\n========== f32x4_calc.h ==========");
     test_f32x4_hadd();
     test_f32x4_abs();
+    test_f32x4_sign();
     test_f32x4_neg();
     test_f32x4_mod();
     test_f32x4_trunc();
@@ -84,9 +86,40 @@ int main()
     test_f32x4_mul_sub();
     test_f32x4_nmul_add();
     test_f32x4_nmul_sub();
+    test_f32x4_neq();
+    test_f32x4_eq();
+    test_f32x4_ge();
+    test_f32x4_gt();
+    test_f32x4_le();
+    test_f32x4_lt();
     test_f32x4_is_nan();
     test_f32x4_is_infinite();
     test_f32x4_is_finite();
+
+    fprintf(stderr, "\n\n========== f32x8_calc.h ==========");
+    test_f32x8_hadd();
+    test_f32x8_abs();
+    test_f32x8_sign();
+    test_f32x8_neg();
+    test_f32x8_mod();
+    test_f32x8_trunc();
+    test_f32x8_round();
+    test_f32x8_floor();
+    test_f32x8_ceil();
+    test_f32x8_clamp();
+    test_f32x8_mul_add();
+    test_f32x8_mul_sub();
+    test_f32x8_nmul_add();
+    test_f32x8_nmul_sub();
+    test_f32x8_neq();
+    test_f32x8_eq();
+    test_f32x8_ge();
+    test_f32x8_gt();
+    test_f32x8_le();
+    test_f32x8_lt();
+    test_f32x8_is_nan();
+    test_f32x8_is_infinite();
+    test_f32x8_is_finite();
 
     test_report();
 
