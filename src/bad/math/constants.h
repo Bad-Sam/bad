02e0893f07709debbcf643ab1dae5b80b34e3a39
Bad-Sam <sami.amara@pm.me>
2022-05-03 22:30:07 +0200

    Added a few common constants across the project

---
+++ src/bad/math/constants.h
@@ -0,0 +1,17 @@
+#ifndef BAD_CONSTANTS_H
+#define BAD_CONSTANTS_H
+
+#include <bad/bad.h>
+#include <bad/types.h>
+
+BAD_NAMESPACE_START
+
+const f32 pi          = 3.1415927410125732421875f;
+const f32 half_pi     = 1.57079637050628662109375f;
+const f32 euler       = 2.71828174591064453125f;
+const f32 pi_rcp      = .3183098733425140380859375f;
+const f32 half_pi_rcp = .63661977236758134308f;
+
+BAD_NAMESPACE_END
+
+#endif
