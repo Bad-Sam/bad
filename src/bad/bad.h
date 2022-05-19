43f14302ddbc249a4b4cb1ac065227163e3fa1e6
Bad-Sam <sami.amara@pm.me>
2022-05-19 11:28:09 +0200

    Added target language dependent (C or C++) clause to use or not use namespace

--- src/bad/bad.h
+++ src/bad/bad.h
@@ -4,9 +4,11 @@
 #if defined(__cplusplus)
 #   define BAD_NAMESPACE_START namespace bad {
 #   define BAD_NAMESPACE_END }
+#   define BAD_USE_NAMESPACE using namespace bad
 #else
 #   define BAD_NAMESPACE_START
 #   define BAD_NAMESPACE_END
+#   define BAD_USE_NAMESPACE
 #endif
 
 #if defined(__SSE__) || defined(__ARM_NEON_FP)
