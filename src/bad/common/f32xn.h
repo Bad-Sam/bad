201911a19ce7f679511494afa6a7817832cc22f6
Bad-Sam <sami.amara@pm.me>
2022-05-19 09:06:45 +0200

    Put maskn, f32xn and their functions up to date

--- src/bad/common/f32xn.h
+++ src/bad/common/f32xn.h
@@ -5,17 +5,18 @@
 
 // f32xn is the widest available vector
 #if defined(BAD_FORCE_WIDEST_VEC) || defined(BAD_FORCE_8WAY_VEC)
-#   define f32xn_width 8
+#   define f32xn_width 8u
 #   include "f32x8.h"
 #elif defined(BAD_FORCE_TINIEST_VEC) || defined(BAD_FORCE_4WAY_VEC)
-#   define f32xn_width 4
+#   define f32xn_width 4u
 #   include "f32x4.h"
 #else // By default, use the widest available SIMD intrinsics type
 #   if defined(__AVX__)
-#       define f32xn_width 8
+#       define f32xn_width 8u
 #       include "f32x8.h"
 #   else
-#       define f32xn_width 4
+#       define f32xn_width 4u
+#       warning f32xn_width = 4u
 #       include "f32x4.h"
 #   endif
 #endif
@@ -23,32 +24,40 @@
 
 BAD_NAMESPACE_START
 
-#if f32xn_width == 8
+#if (f32xn_width == 8u)
     typedef f32x8 f32xn;
-#   define f32xn_any        f32x8_any
-#   define f32xn_zero       f32x8_zero
-#   define f32xn_one        f32x8_one
-#   define f32xn_load       f32x8_load
-#   define f32xn_loadu      f32x8_loadu
-#   define f32xn_set        f32x8_set
-#   define f32xn_set1       f32x8_set1
-#   define f32xn_store      f32x8_store
-#   define f32xn_storeu     f32x8_storeu
-#   define f32xn_blend      f32x8_blend
-#   define f32xn_as_maskn f32x8_as_mask256
+
+#   define f32xn_any         f32x8_any
+#   define f32xn_zero        f32x8_zero
+#   define f32xn_half        f32x8_half
+#   define f32xn_one         f32x8_one
+#   define f32xn_two         f32x8_two
+#   define f32xn_load        f32x8_load
+#   define f32xn_loadu       f32x8_loadu
+#   define f32xn_set_all     f32x8_set_all
+#   define f32xn_set_lower   f32x8_set_lower
+#   define f32xn_store       f32x8_store
+#   define f32xn_storeu      f32x8_storeu
+#   define f32xn_store_lower f32x8_store_lower
+#   define f32xn_blend       f32x8_blend
+#   define f32xn_as_maskn    f32x8_as_mask256
 #else
     typedef f32x4 f32xn;
-#   define f32xn_any        f32x4_any
-#   define f32xn_zero       f32x4_zero
-#   define f32xn_one        f32x4_one
-#   define f32xn_load       f32x4_load
-#   define f32xn_loadu      f32x4_loadu
-#   define f32xn_set        f32x4_set
-#   define f32xn_set1       f32x4_set1
-#   define f32xn_store      f32x4_store
-#   define f32xn_storeu     f32x4_storeu
-#   define f32xn_blend      f32x4_blend
-#   define f32xn_as_maskn f32x4_as_mask128
+    
+#   define f32xn_any         f32x4_any
+#   define f32xn_zero        f32x4_zero
+#   define f32xn_half        f32x4_half
+#   define f32xn_one         f32x4_one
+#   define f32xn_two         f32x4_two
+#   define f32xn_load        f32x4_load
+#   define f32xn_loadu       f32x4_loadu
+#   define f32xn_set_all     f32x4_set_all
+#   define f32xn_set_lower   f32x4_set_lower
+#   define f32xn_store       f32x4_store
+#   define f32xn_storeu      f32x4_storeu
+#   define f32xn_store_lower f32x4_store_lower
+#   define f32xn_blend       f32x4_blend
+#   define f32xn_as_maskn    f32x4_as_mask128
 #endif
 
 BAD_NAMESPACE_END
--- src/bad/common/maskn.h
+++ src/bad/common/maskn.h
@@ -3,29 +3,90 @@
 
 #include <bad/bad.h>
 
-BAD_NAMESPACE_START
-
 // maskn is the widest available vector
 #if defined(BAD_FORCE_WIDEST_VEC) || defined(BAD_FORCE_8WAY_VEC)
-    static const s32 maskn_width = 8;
-    typedef maskn mask256;
-#elif defined(BAD_FORCE_4WAY_VEC)
-    static const s32 maskn_width = 4;
-    typedef maskn mask128;
-#elif defined(BAD_FORCE_TINIEST_VEC) || defined(BAD_FORCE_1WAY_VEC)
-    static const s32 maskn_width = 1;
-    typedef maskn f32;
+#   define maskn_width 8u
+#   include "mask256.h"
+#elif defined(BAD_FORCE_TINIEST_VEC) || defined(BAD_FORCE_4WAY_VEC)
+#   define maskn_width 4u
+#   include "mask128.h"
 #else // By default, use the widest available SIMD intrinsics type
 #   if defined(__AVX__)
-        static const s32 maskn_width = 8;
-        typedef maskn mask256;
-#   elif defined(__SSE__) || defined(__ARM_NEON_FP)
-        static const s32 maskn_width = 4;
-        typedef maskn mask128;
+#       define maskn_width 8u
+#       include "mask256.h"
 #   else
-        static const s32 maskn_width = 1;
-        typedef maskn f32;
+#       define maskn_width 4u
+#       include "mask128.h"
 #   endif
+#endif
+
+BAD_NAMESPACE_START
+
+#if (maskn_width == 8u) 
+    typedef mask256 maskn;
+
+#   define maskn_load           mask256_load
+#   define maskn_loadu          mask256_loadu
+#   define maskn_set_all        mask256_set_all
+#   define maskn_store          mask256_store
+#   define maskn_storeu         mask256_storeu
+
+#   define maskn_zero           mask256_zero
+#   define maskn_all1           mask256_all1
+#   define maskn_value32        mask256_value32
+#   define maskn_highbit32      mask256_highbit32
+#   define maskn_lowbit32       mask256_lowbit32
+#   define maskn_exponent32     mask256_exponent32
+
+#   define maskn_keep_highbit32 mask256_keep_highbit32
+#   define maskn_keep_lowbit32  mask256_keep_lowbit32
+#   define maskn_shift_left32   mask256_shift_left32
+#   define maskn_shift_right32  mask256_shift_right32
+
+#   define maskn_eq             mask256_eq
+#   define maskn_neq            mask256_neq
+#   define maskn_gt             mask256_gt
+
+#   define maskn_and            mask256_and
+#   define maskn_and_not        mask256_and_not
+#   define maskn_or             mask256_or
+#   define maskn_xor            mask256_xor
+#   define maskn_not            mask256_not
+
+#   define maskn_as_f32xn       mask256_as_f32x8
+#else
+    typedef mask128 maskn;
+
+#   define maskn_load           mask128_load
+#   define maskn_loadu          mask128_loadu
+#   define maskn_set_all        mask128_set_all
+#   define maskn_store          mask128_store
+#   define maskn_storeu         mask128_storeu
+
+#   define maskn_zero           mask128_zero
+#   define maskn_all1           mask128_all1
+#   define maskn_value32        mask128_value32
+#   define maskn_highbit32      mask128_highbit32
+#   define maskn_lowbit32       mask128_lowbit32
+#   define maskn_exponent32     mask128_exponent32
+
+#   define maskn_keep_highbit32 mask128_keep_highbit32
+#   define maskn_keep_lowbit32  mask128_keep_lowbit32
+#   define maskn_shift_left32   mask128_shift_left32
+#   define maskn_shift_right32  mask128_shift_right32
+
+#   define maskn_eq             mask128_eq
+#   define maskn_neq            mask128_neq
+#   define maskn_gt             mask128_gt
+
+#   define maskn_and            mask128_and
+#   define maskn_and_not        mask128_and_not
+#   define maskn_or             mask128_or
+#   define maskn_xor            mask128_xor
+#   define maskn_not            mask128_not
+
+#   define maskn_as_f32xn       mask128_as_f32x8
+
 #endif
 
 BAD_NAMESPACE_END
